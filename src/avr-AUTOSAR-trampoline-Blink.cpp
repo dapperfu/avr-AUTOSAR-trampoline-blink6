e3a071e5a2bcf9aa9787e2227f6f2660e7a5d014
jed <jed@m6700.lan>
2019-08-27 02:17:11 -0400

    .

--- src/avr-AUTOSAR-trampoline-Blink.cpp
+++ src/avr-AUTOSAR-trampoline-Blink.cpp
@@ -4,45 +4,45 @@ extern "C" {
 #include "tpl_app_config.h"
 #include "tpl_os.h" //set the OSEK ISR version.
 }
-#include "math.h"
 
 void setup() {
+
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
-
-  // "Ground" for Interrupts.
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
-
-  // Serial.
-  Serial.begin(115200);
 }
+
 
 TASK(blinkTaskPin8) {
-  static unsigned long long nb = 0;
+  static unsigned char nb = 0;
   digitalWrite(8, nb++ & 0x01);
 }
+
 TASK(blinkTaskPin9) {
-  static unsigned long long nb = 0;
+  static unsigned char nb = 0;
   digitalWrite(9, nb++ & 0x01);
 }
+
 TASK(blinkTaskPin10) {
-  static unsigned long long nb = 0;
+  static unsigned char nb = 0;
   digitalWrite(10, nb++ & 0x01);
 }
+
 TASK(blinkTaskPin11) {
-  static unsigned long long nb = 0;
+  static unsigned char nb = 0;
   digitalWrite(11, nb++ & 0x01);
 }
+
 TASK(blinkTaskPin12) {
-  static unsigned long long nb = 0;
+  static unsigned char nb = 0;
   digitalWrite(12, nb++ & 0x01);
 }
+
 TASK(blinkTaskPin13) {
-  static unsigned long long nb = 0;
+  static unsigned char nb = 0;
   digitalWrite(13, nb++ & 0x01);
 }
+
